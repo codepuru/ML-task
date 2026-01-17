ff437540142a9ea335f2392d45a486d266770d89
codepuru <tompurumeow@gmail.com>
2026-01-17 23:31:39 +0530

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,13 +2,15 @@
 #Purushrut Chauhan 25BEC080
 import random
 p = random.randint(0 , 50)
-
 print('WELCOME to Guess a Random Number BETWEEN 1 and 50')
 print('ONLY 3 CHANCES')
 
 chance = 3
 while chance>0:
-    guess=input('Select a number : \n')
+    guess=int(input('Select a number : \n'))    
+    if guess>50 or guess<0:
+        print('Number is out of range')
+        break
     if guess==p:
         print('That is the correct choice')
         break
@@ -16,7 +18,6 @@ while chance>0:
         print('Try Again !!')
         chance -= 1
         print(chance , 'Chances left')
-        
 print('GAME OVER !!!')        
         
 print(p, 'is the correct answer')
